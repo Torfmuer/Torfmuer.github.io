d13c81c347e564e603b77190a83dc21e1b188f6b
ofoster24 <ofoster24@my.whitworth.edu>
2023-09-25 10:01:02 -0700

    added menu and menu styles

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 document.getElementById("cards").onmousemove = e => {
-    for(const card of document.getElementsByClassName("card")) {
-      const rect = card.getBoundingClientRect(),
-            x = e.clientX - rect.left,
-            y = e.clientY - rect.top;
-  
-      card.style.setProperty("--mouse-x", `${x}px`);
-      card.style.setProperty("--mouse-y", `${y}px`);
-    };
-  }
+  for(const card of document.getElementsByClassName("card")) {
+    const rect = card.getBoundingClientRect(),
+          x = e.clientX - rect.left,
+          y = e.clientY - rect.top;
+
+    card.style.setProperty("--mouse-x", `${x}px`);
+    card.style.setProperty("--mouse-y", `${y}px`);
+  };
+}
+
+function men_rote(x) {
+  x.classList.toggle("change");
+}
